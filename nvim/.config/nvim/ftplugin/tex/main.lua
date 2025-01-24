f065fb3ad2f6754833568a08136968b9e148c63d
tgoldenphoenix <anhaophamx@gmail.com>
2025-01-24 08:12:10 +0700

    add something

---
+++ nvim/.config/nvim/ftplugin/tex/main.lua
@@ -0,0 +1,7 @@
+vim.cmd[[
+" Use `<localleader>c` to trigger continuous compilation...
+nmap <localleader>c <Plug>(vimtex-compile)
+
+" This is for testing
+" nmap <localleader>c <Cmd>echo "anhao muon chich kim phuong"<CR>
+]]
--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -1,5 +1,19 @@
 -- set all global VimTeX options (those that start with g:)
 -- These are configuration that should be set before VimTeX loads.
+vim.cmd[[
+" Don't open QuickFix for warning messages if no errors are present
+let g:vimtex_quickfix_open_on_warning = 0
+
+" Filter out some compilation warning messages from QuickFix display
+let g:vimtex_quickfix_ignore_filters = [
+    \ 'Underfull \\hbox',
+    \ 'Overfull \\hbox',
+    \ 'LaTeX Warning: .\+ float specifier changed to',
+    \ 'LaTeX hooks Warning',
+    \ 'Package siunitx Warning: Detected the "physics" package:',
+    \ 'Package hyperref Warning: Token not allowed in a PDF string',
+    \]
+]]
 
 return {
     "lervag/vimtex",
