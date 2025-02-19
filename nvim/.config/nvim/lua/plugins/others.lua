6777ef4deb3d44cc296c54a8b18933fdff3a7362
tgoldenphoenix <anhaophamx@gmail.com>
2025-02-19 21:52:21 +0700

    kim

--- nvim/.config/nvim/lua/plugins/others.lua
+++ nvim/.config/nvim/lua/plugins/others.lua
@@ -15,7 +15,7 @@ return {
         "iamcco/markdown-preview.nvim",
         -- cmd key for Lazy-load on command
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
+        build = "cd app && npm install",
         init = function()
           vim.g.mkdp_filetypes = { "markdown" }
         end,
