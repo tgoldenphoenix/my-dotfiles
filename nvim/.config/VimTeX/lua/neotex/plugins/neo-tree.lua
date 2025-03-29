4d03db1b6913b458220ac06f14dd1db3e35a9ca9
tgoldenphoenix <anhaophamx@gmail.com>
2025-03-29 07:17:29 +0700

    some changes

--- nvim/.config/VimTeX/lua/neotex/plugins/neo-tree.lua
+++ nvim/.config/VimTeX/lua/neotex/plugins/neo-tree.lua
@@ -1,33 +1,57 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
-    -- opts = {},
-    config = function()
-        -- keymaps
-        vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", {desc = "Toggle Neotre[e]"})
-        vim.keymap.set("v", "<leader>e", "<cmd>Neotree toggle<CR>", {desc = "Toggle Neotre[e]"})
-        
-        require("neo-tree").setup({
-          filesystem = {
-            filtered_items = {
-              hide_by_name = {},
-              hide_by_pattern = {},
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_by_name = {},
+        hide_by_pattern = {},
 
-              always_show = {},
-              always_show_by_pattern = { -- remains visible even if other settings would normally hide it
-                -- "*.txt",
-              },
-            }
-          },
-        })
-    end,
+        always_show = {},
+        always_show_by_pattern = { -- remains visible even if other settings would normally hide it
+          -- "*.txt",
+        },
+      }, -- filtered_items
+    },
+
+    window = {
+      mappings = {
+        ["l"] = "open", -- open node
+        ["h"] = "close_node",
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "Open with System Application",
+        },
+        ["P"] = { "toggle_preview", config = { use_float = false } },
+        ["z"] = "close_all_nodes",
+            --["Z"] = "expand_all_nodes",
+      },
+    },
+
+  },
+
+  keys = {
+    {"<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neotre[e]"},
+  },
+
+  -- config = function()
+  -- end,
 }
